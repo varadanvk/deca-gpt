12a80d7906da636eada6dce8bcc362c3852eebb3
varadanvk <126513116+varadanvk@users.noreply.github.com>
2025-02-08 17:32:57 -0800

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Deca-GPT',
+  title: 'DECA GPT',
+  description: 'Deca-GPT is a tool that helps students prepare for the DECA role-play competition.',
+  icons: {
+    icon: '/logo.png',
+  },
 }
 
 export default function RootLayout({
